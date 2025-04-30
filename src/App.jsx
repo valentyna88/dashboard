0847a059c274b88dae26037c8e931b318c23727d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-30 13:04:06 +0300

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import AccountPage from './pages/AccountPage';
+import Layout from './Layout/Layout';
 import './App.css';
 
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="account/:id" element={<AccountPage />} />
+      </Route>
+      <Route path="*" element={<div>404 - Page not found</div>} />
+    </Routes>
+  );
 };
 
 export default App;
